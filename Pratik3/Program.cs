716393c2b8782cd3a8ccf9a5ba13e7e1c5890276
erkankaradag <erkankaradagcom@gmail.com>
2024-12-17 16:00:04 +0300

    Add project files.

---
+++ Pratik3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace Pratik3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Lüften bir sayı giriniz: ");
+            int sayi = Convert.ToInt32(Console.Read());
+
+            if (sayi == 10)
+            {
+                Console.WriteLine("Girdiğiniz sayı 10'a eşittir.");
+            }
+            else if (sayi < 10)
+            {
+                Console.WriteLine("Girilen sayı 10'dan küçüktür.");
+            }
+            else
+            {
+                Console.WriteLine("Girilen sayı 10'dan büyüktür.");
+            }
+            if (sayi % 2 == 0)
+            {
+                Console.WriteLine("Sayı çifttir.");
+            }
+            else
+            {
+                Console.WriteLine("Sayı tektir.");
+            }
+        }
+    }
+}
